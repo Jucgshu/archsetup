03e39bcd4e8157a5a6e82382c936d4c0146a93ce
Jucgshu <Jucgshu@users.noreply.github.com>
2022-08-23 21:58:36 +0200

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -16,6 +16,15 @@ sudo sed -i -e 's|[# ]*IgnoreLid[ ]*=[ ]*.*|IgnoreLid=true|g' /etc/UPower/UPower
 # ------------------------------------------------------------------------
 
 echo
+echo "Enable some nice Reflector options"
+
+sudo sed -i -e 's|[# ]*--country[ ]* [ ]*.*|--country France,Germany|g' /etc/xdg/reflector/reflector.conf
+sudo sed -i -e 's|[# ]*--protocol[ ]* [ ]*.*|--protocol https|g' /etc/xdg/reflector/reflector.conf
+sudo sed -i -e 's|[# ]*--latest[ ]* [ ]*.*|--latest 5|g' /etc/xdg/reflector/reflector.conf
+
+# ------------------------------------------------------------------------
+
+echo
 echo "Add TRIM option to the SSD drives"
 
 sudo sed -i 's/ssd,/ssd,discard=async,/g' /etc/fstab
@@ -32,6 +41,24 @@ echo
 echo "Apply GNOME settings"
 
 gsettings set org.gnome.shell.app-switcher current-workspace-only true
+gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 2680
+gsettings set org.gnome.desktop.interface clock-show-date true
+gsettings set org.gnome.desktop.calendar show-weekdate true
+gsettings set org.gnome.desktop.peripherals.touchpad click-method 'none'
+gsettings set org.gnome.desktop.interface icon-theme Papirus
+# ------------------------------------------------------------------------
+
+echo
+echo "Install Yay"
+
+cd ~/ && git clone https://aur.archlinux.org/yay.git && cd yay
+# ------------------------------------------------------------------------
+
+sudo systemctl start reflector.service
+sudo systemctl enable --now rngd.service
+sudo systemctl enable --now cpupower.service
+sudo systemctl enable --now firewalld.service
+systemctl --user start syncthing.service
 
 echo "Done!"
 echo
