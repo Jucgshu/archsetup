073cd12007a73acdb1ec39b7f313cdc14f330fbf
Jucgshu <balayop@yahoo.fr>
2022-08-25 09:18:24 +0200

    Add Firefox profile

--- archsetup.sh
+++ archsetup.sh
@@ -2,19 +2,12 @@
 # ------------------------------------------------------------------------
 
 echo
-echo "Fix buggy lid buggy firmware by delegating lid close event to Systemd"
-
-sudo sed -i -e 's|[# ]*HandleLidSwitch[ ]*=[ ]*.*|HandleLidSwitch=suspend|g' /etc/systemd/logind.conf
-sudo sed -i -e 's|[# ]*IgnoreLid[ ]*=[ ]*.*|IgnoreLid=true|g' /etc/UPower/UPower.conf
-
-# ------------------------------------------------------------------------
-
-echo
-echo "Enable some nice Reflector options"
+echo "Enable Reflector"
 
 sudo sed -i -e 's|[# ]*--country[ ]* [ ]*.*|--country France,Germany|g' /etc/xdg/reflector/reflector.conf
 sudo sed -i -e 's|[# ]*--protocol[ ]* [ ]*.*|--protocol https|g' /etc/xdg/reflector/reflector.conf
 sudo sed -i -e 's|[# ]*--latest[ ]* [ ]*.*|--latest 5|g' /etc/xdg/reflector/reflector.conf
+sudo systemctl start reflector.service
 
 # ------------------------------------------------------------------------
 
@@ -26,9 +19,18 @@ sudo sed -i 's/ssd,/ssd,discard=async,/g' /etc/fstab
 # ------------------------------------------------------------------------
 
 echo
-echo "Add power savings options to boot"
+echo "Enable power savings options"
 
 sudo sed -i 's/$/ i915.enable_rc6=1 i915.enable_psr=2/' /boot/loader/entries/*linux.conf
+sudo systemctl enable --now cpupower.service
+
+# ------------------------------------------------------------------------
+
+echo
+echo "Fix buggy lid buggy firmware by delegating lid close event to Systemd"
+
+sudo sed -i -e 's|[# ]*HandleLidSwitch[ ]*=[ ]*.*|HandleLidSwitch=suspend|g' /etc/systemd/logind.conf
+sudo sed -i -e 's|[# ]*IgnoreLid[ ]*=[ ]*.*|IgnoreLid=true|g' /etc/UPower/UPower.conf
 
 # ------------------------------------------------------------------------
 
@@ -41,6 +43,15 @@ gsettings set org.gnome.desktop.interface clock-show-date true
 gsettings set org.gnome.desktop.calendar show-weekdate true
 gsettings set org.gnome.desktop.peripherals.touchpad click-method 'none'
 gsettings set org.gnome.desktop.interface icon-theme Papirus
+
+# ------------------------------------------------------------------------
+
+echo
+echo "Start with a clean Firefox profile"
+
+read -p "Enter the name of your Firefox profile: " firefox
+firefox -CreateProfile $firefox
+cp ./Firefox/prefs.js ~/.mozilla/firefox/*.$firefox
 
 # ------------------------------------------------------------------------
 
@@ -80,12 +91,10 @@ sudo systemctl enable --now unbound.service
 # ------------------------------------------------------------------------
 
 echo
-echo "Enable services"
+echo "Enable other services"
 
 sudo systemctl enable --now systemd-resolved.service
-sudo systemctl start reflector.service
 sudo systemctl enable --now rngd.service
-sudo systemctl enable --now cpupower.service
 sudo systemctl enable --now firewalld.service
 systemctl --user start syncthing.service
 
