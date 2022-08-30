8731e35eb3de58845e106aa908c95bc9bbb4d986
Jucgshu <Jucgshu@users.noreply.github.com>
2022-08-30 19:22:18 +0200

    Fix connection not available

--- 0_pre_setup.sh
+++ 0_pre_setup.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 # ------------------------------------------------------------------------
 
-# Connect to the network
-read -p "Enter the name of your SSID: " SSID
-read -p "Enter passphrase: " PASSPHRASE
-iwctl --$PASSPHRASE station wlan0 connect $SSID &&
-echo "Wifi connected"
-
 # Enable Reflector
 sed -i -e 's|[# ]*--country[ ]* [ ]*.*|--country France,Germany|g' /etc/xdg/reflector/reflector.conf &&
 sed -i -e 's|[# ]*--protocol[ ]* [ ]*.*|--protocol https|g' /etc/xdg/reflector/reflector.conf &&
@@ -18,11 +12,6 @@ sed -i -e 's|[# ]*Color.*|Color|g' /etc/pacman.conf &&
 sed -i -e 's|[# ]*ParallelDownloads[ ]* = [ ]*.*|ParallelDownloads = 5|g' /etc/pacman.conf &&
 pacman -Syy &&
 echo "Pacman OK"
-
-# Install Arch install tools
-pacman -S --noconfirm git &&
-git clone https://github.com/Jucgshu/archinstall &&
-echo "Arch install tools installed"
 
 # Start Archinstall script
 archinstall --config archinstall/user_configuration.json
