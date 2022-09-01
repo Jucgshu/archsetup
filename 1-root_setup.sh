401d29f22737b8316575dd944d6b788869c5d5f6
Jucgshu <balayop@yahoo.fr>
2022-09-01 14:52:06 +0200

    Move user creation to root_setup & rename files

--- 1-root_setup.sh
+++ 1-root_setup.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 # ------------------------------------------------------------------------
 
+createUser () {
+  echo "Enable Reflector"
+  systemctl enable --now systemd-homed.service
+  read -p "User you wish to create" MYUSER
+  homectl create $MYUSER --shell=/usr/bin/zsh --member-of=wheel --storage=subvolume
+  sed -i "/WaylandEnable/aAutomaticLogin=$MYUSER" /etc/gdm/custom.conf
+}
+
+# ------------------------------------------------------------------------
+
 enableUnbound () {
   echo "Enable Unbound"
   cp ./archlinux/unbound.conf /etc/unbound/
@@ -91,6 +101,7 @@ setUserSettings () {
 
 # ------------------------------------------------------------------------
 
+createUser
 enableUnbound
 enableReflector
 setNetworkSettings
